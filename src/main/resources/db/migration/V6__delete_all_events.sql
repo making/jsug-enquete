ac651257f7dcb54ae51dc24f9ab02bfd9504a162
Toshiaki Maki <tmaki@pivotal.io>
2020-12-23 14:49:13 +0900

    reset db migration

---
+++ src/main/resources/db/migration/V6__delete_all_events.sql
@@ -0,0 +1,4 @@
+DELETE FROM session_speaker_display_names;
+DELETE FROM session_speakers;
+DELETE FROM session;
+DELETE FROM seminar;
---
+++ src/main/resources/db/migration/V7__add_spring_fest_2020.sql
@@ -0,0 +1,47 @@
+INSERT INTO seminar(seminar_id, seminar_date, seminar_name) VALUES (0x00000000000000000000000020201217, '2020-12-17', 'Spring Fest 2020');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x0000000000000000000000a120201217, 'A1. Spring CloudユーザーのためのDapr入門', 0x00000000000000000000000020201217);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x0000000000000000000000a120201217, '谷本 心');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x0000000000000000000000a220201217, 'A2. AWSで作るマイクロサービスアプリケーション', 0x00000000000000000000000020201217);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x0000000000000000000000a220201217, '川畑 光平');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x0000000000000000000000a320201217, 'A3. R2DBC入門 - 概要とエコシステム、今後の動向', 0x00000000000000000000000020201217);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x0000000000000000000000a320201217, '露久保 忠也');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x0000000000000000000000a420201217, 'A4. Cloud Nativeな決済サービスの開発と2年間の歩み', 0x00000000000000000000000020201217);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x0000000000000000000000a420201217, '鈴木順也');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x0000000000000000000000a520201217, 'A5. JPA × jMolecules で DDD をコードで表現する', 0x00000000000000000000000020201217);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x0000000000000000000000a520201217, '清田 武史');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x0000000000000000000000a520201217, '古家 優');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x0000000000000000000000a620201217, 'A6. モノリスからマイクロサービスへの移行 〜ストラングラーパターンの検証〜', 0x00000000000000000000000020201217);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x0000000000000000000000a620201217, '横井 一輝');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x0000000000000000000000a720201217, 'A7. What\'s New in Spring 5.3 and Spring Boot 2.4', 0x00000000000000000000000020201217);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x0000000000000000000000a720201217, '槙 俊明');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x0000000000000000000000b120201217, 'B1. 今こそ知りたいSpringエコシステム', 0x00000000000000000000000020201217);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x0000000000000000000000b120201217, '槙 俊明');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x0000000000000000000000b220201217, 'B2. 「今こそ知りたい」を聞くために今こそ知りたいSpring Boot', 0x00000000000000000000000020201217);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x0000000000000000000000b220201217, '長谷川 裕一');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x0000000000000000000000b320201217, 'B3. 今こそ知りたいSpring DI x AOP', 0x00000000000000000000000020201217);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x0000000000000000000000b320201217, '多田 真敏');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x0000000000000000000000b420201217, 'B4. 今こそ知りたいSpring Web', 0x00000000000000000000000020201217);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x0000000000000000000000b420201217, '伊藤 司');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x0000000000000000000000b520201217, 'B5. 今こそ知りたいSpring Security', 0x00000000000000000000000020201217);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x0000000000000000000000b520201217, '土岐 孝平');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x0000000000000000000000b620201217, 'B6. 今こそ知りたいSpring Batch', 0x00000000000000000000000020201217);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x0000000000000000000000b620201217, '橋本 直樹');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x0000000000000000000000b720201217, 'B7. 今こそ知りたいSpring Data', 0x00000000000000000000000020201217);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x0000000000000000000000b720201217, '小川 岳史');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x0000000000000000000000b820201217, 'B8. 今こそ知りたいSpring Test', 0x00000000000000000000000020201217);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x0000000000000000000000b820201217, '進藤 遼');
