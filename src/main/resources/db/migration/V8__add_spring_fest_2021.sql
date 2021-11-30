4121b51cb2d4ce0ffd36788d03e4a2b921010792
Toshiaki Maki <tmaki@vmware.com>
2021-12-01 02:12:39 +0900

    indexed

--- src/main/resources/db/migration/V8__add_spring_fest_2021.sql
+++ src/main/resources/db/migration/V8__add_spring_fest_2021.sql
@@ -1,25 +1,25 @@
 INSERT INTO seminar(seminar_id, seminar_date, seminar_name) VALUES (0x00000000000000000000000020211203, '2021-12-03', 'Spring Fest 2021');
 --
-INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000120211203, 'Cloud Native Buildpacksで作る至高のコンテナイメージ for Spring Boot ~ おそらくあなたの知らないBuildpack活用法 ~', 0x00000000000000000000000020211203);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000120211203, '1. Cloud Native Buildpacksで作る至高のコンテナイメージ for Spring Boot ~ おそらくあなたの知らないBuildpack活用法 ~', 0x00000000000000000000000020211203);
 INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000120211203, '槙 俊明');
 --
-INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000220211203, 'Spring Web MVCのマイクロサービス化の経緯と今', 0x00000000000000000000000020211203);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000220211203, '2. Spring Web MVCのマイクロサービス化の経緯と今', 0x00000000000000000000000020211203);
 INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000220211203, '平井 一史');
 --
-INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000320211203, '怖くないSpring Bootのオートコンフィグレーション', 0x00000000000000000000000020211203);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000320211203, '3. 怖くないSpring Bootのオートコンフィグレーション', 0x00000000000000000000000020211203);
 INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000320211203, '土岐 孝平');
 --
-INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000420211203, 'LINEの予約プラットフォームにおけるSpring WebFluxとKotlin Coroutineの活用事例', 0x00000000000000000000000020211203);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000420211203, '4. LINEの予約プラットフォームにおけるSpring WebFluxとKotlin Coroutineの活用事例', 0x00000000000000000000000020211203);
 INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000420211203, '小山 淳平');
 --
-INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000520211203, 'プロになるためのSpring上級知識', 0x00000000000000000000000020211203);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000520211203, '5. プロになるためのSpring上級知識', 0x00000000000000000000000020211203);
 INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000520211203, '多田 真敏');
 --
-INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000620211203, '決済システムで学ぶレジリエントなサービスのいろは', 0x00000000000000000000000020211203);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000620211203, '6. 決済システムで学ぶレジリエントなサービスのいろは', 0x00000000000000000000000020211203);
 INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000620211203, '高野　はいね');
 --
-INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000720211203, 'Spring Batchでサブスク1億件のクレカ決済処理', 0x00000000000000000000000020211203);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000720211203, '7. Spring Batchでサブスク1億件のクレカ決済処理', 0x00000000000000000000000020211203);
 INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000720211203, '鈴木 隆志');
 --
-INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000820211203, 'Spring ObservabilityとこれからのMicrometer', 0x00000000000000000000000020211203);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000820211203, '8. Spring ObservabilityとこれからのMicrometer', 0x00000000000000000000000020211203);
 INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000820211203, 'Tommy Ludwig');
