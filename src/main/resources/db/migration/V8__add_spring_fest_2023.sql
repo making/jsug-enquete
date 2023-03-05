bca3e42ef7beefe8a361c0cedc97e4c62e2d4e0b
ikeyat <tm.ikeya@gmail.com>
2023-03-05 23:51:02 +0900

    Add records for spring fest 2023

---
+++ src/main/resources/db/migration/V8__add_spring_fest_2023.sql
@@ -0,0 +1,28 @@
+INSERT INTO seminar(seminar_id, seminar_date, seminar_name) VALUES (0x00000000000000000000000020230317, '2023-03-17', 'Spring Fest 2023');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000120230317, '1. TBD', 0x00000000000000000000000020230317);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000120230317, '槙 俊明');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000220230317, '2. Spring Boot 3.0 へのアップデートのハマりどころ', 0x00000000000000000000000020230317);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000220230317, '森田 大翼');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000320230317, '3. 詳解Springトランザクション 〜初級から上級まで〜', 0x00000000000000000000000020230317);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000320230317, '多田 真敏');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000420230317, '4. Spring Securityでログイン機能を作る', 0x00000000000000000000000020230317);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000420230317, 'うらがみ');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000520230317, '5. TBD', 0x00000000000000000000000020230317);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000520230317, '大塚 優斗');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000620230317, '6. Introducing AuthN/AuthZ for Microservice Migration with Spring Security ❤️ OAuth 2.0', 0x00000000000000000000000020230317);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000620230317, '小山 純');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000720230317, '7. Spring とAWS サービスを活用して実現する安全なデプロイメント', 0x00000000000000000000000020230317);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000720230317, '金森 政雄, 林 政利');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000820230317, '8. What''s new in Spring Batch 5', 0x00000000000000000000000020230317);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000820230317, '池谷 智行');
+--
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x00000000000000000000000920230317, '9. TBD', 0x00000000000000000000000020230317);
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x00000000000000000000000920230317, 'Tommy Ludwig');
